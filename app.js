9d770908a9c2fbf83a67becdeb32e3b4edfa661d
José Miranda <josemiranda989@gmail.com>
2023-06-09 02:13:55 -0300

    add

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const session = require('express-session');
 const cookies = require('cookie-parser');
+const path = require('path');
+const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware')
+
+/*const mainRoutes = require ('./routes/mainRoutes');*/
+const userRoutes = require ('./routes/userRoutes');
 
 const app = express();
 
-const userLoggedMiddleware = require('./middlewares/userLoggedMiddleware')
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "./public")));
 
 app.use(session({
     secret: "Shhh",
@@ -16,19 +22,14 @@ app.use(userLoggedMiddleware);
 
 app.use(cookies());
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-const path = require('path');
 
-app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(3000, () => console.log('Servidor levantado en el puerto 3000'));
 
-app.set('view engine', 'ejs');
-
-/*const mainRoutes = require ('./routes/mainRoutes');*/
-const userRoutes = require ('./routes/userRoutes');
 
 /*app.use('/', mainRoutes);*/
 app.use('/user', userRoutes); 
 
+app.listen(3000, () => console.log('Servidor levantado en el puerto 3000'));
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,96 +1,100 @@
-const { validationResult } = require ('express-validator');
+const { validationResult } = require("express-validator");
 
-const user = require('../model/user');
+const user = require("../model/user");
 
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const controller = {
+  home: (req, res) => {
+    return res.render("index");
+  },
 
-    home: (req, res) => {
-        return res.render('index');
-    },
+  register: (req, res) => {
+    return res.render("userRegisterForm");
+  },
+  processRegister: (req, res) => {
+    const resultValidation = validationResult(req);
 
-    register: (req, res) => {
-        return res.render('userRegisterForm');
-    },
-    processRegister: (req, res) => {
+    if (resultValidation.errors.length > 0) {
+      return res.render("userRegisterForm", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+    }
 
-            const resultValidation = validationResult(req);
-            
-            if (resultValidation.errors.length > 0) {
-                return res.render('userRegisterForm', {
-                    errors: resultValidation.mapped(),
-                    oldData: req.body
-                })
-            }
+    let userInDB = user.findByField("email", req.body.email);
 
-            let userInDB = user.findByField('email', req.body.email);
+    if (userInDB) {
+      return res.render("userRegisterForm", {
+        errors: { email: { msg: "Este email ya está registrado" } },
+        oldData: req.body,
+      });
+    }
 
-            if (userInDB) {
-                return res.render('userRegisterForm', {
-                    errors: {email: {msg: 'Este email ya está registrado'}},
-                    oldData: req.body})
-            }
+    let userToCreate = {
+      ...req.body,
+      password: bcrypt.hashSync(req.body.password, 10),
+      avatar: req.file.filename,
+    };
 
-            let userToCreate = {
-                ...req.body,
-                password: bcrypt.hashSync(req.body.password, 10),
-                avatar: req.file.filename
-            }
-
-            let userCreated = user.create(userToCreate);
-            return res.redirect('/user/login')
-    },
-    login: (req, res) => {
-        return res.render('userLoginForm');
-    },
+    let userCreated = user.create(userToCreate);
+    return res.redirect("/user/login");
+  },
+  login: (req, res) => {
+    return res.render("userLoginForm");
+  },
     processLogin: (req, res) => {
 
-        let userToLogin = user.findByField('email', req.body.email);
+    let userToLogin = user.findByField("email", req.body.email);
 
-        if (userToLogin){
-            let isOkPass = bcrypt.compareSync(req.body.password, userToLogin.password)
-            if (isOkPass) {
-                delete userToLogin.password;
-                req.session.userLogged = userToLogin;
 
-                if(req.body.remember_user){
-                    res.cookie('userEmail', req.body.email, { maxAge: 1000 * 600 })
-                }
+    if (userToLogin) {
+      let isOkPass = bcrypt.compareSync(
+        req.body.password,
+        userToLogin.password
+      );
 
-                return res.redirect('/user/profile')
-            }
-            return res.render('userLoginForm', {
-                errors: {
-                    email: {
-                    msg: 'Las credenciales son inválidas'
-                }
-            }
-            });
+      if (isOkPass) {
+        delete userToLogin.password;
+        req.session.userLogged = userToLogin;
+
+        if (req.body.remember_user) {
+          res.cookie("userEmail", req.body.email, { maxAge: 1000 * 600 });
         }
-        
-        return res.render('userLoginForm', {
-            errors: {
-                email: {
-                msg: 'No se encuentra este email'
-            }
-        }
-        });
-    },
 
-    profile: (req, res) => {
-        return res.render('userProfile', {
-            user: req.session.userLogged
-        });
-    },
+        return res.redirect("/user/profile");
+      }
+      return res.render("userLoginForm", {
+        errors: {
+          email: {
+            msg: "Las credenciales son inválidas",
+          },
+        },
+      });
+      }
+      
 
-    logout: (req, res) => {
-        res.clearCookie('userEmail')
-        req.session.destroy();
-        console.log(req.session);
-        return res.redirect('/user');
-    }
-}
+    return res.render("userLoginForm", {
+      errors: {
+        email: {
+          msg: "No se encuentra este email",
+        },
+      },
+    });
+  },
+
+  profile: (req, res) => {
+    return res.render("userProfile", {
+      user: req.session.userLogged,
+    });
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("userEmail");
+    req.session.destroy();
+
+    return res.redirect("/user");
+  },
+};
 
 module.exports = controller;
-
